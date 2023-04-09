658695ed5ed04d3c309916aced96fd49d528e022
deBasMan21 <bbuijsen@gmail.com>
2023-04-09 12:29:12 +0200

    update release notifications when updating project

--- config/cron-task.js
+++ config/cron-task.js
@@ -1,30 +1,6 @@
 module.exports = {
-  /**
-   * Scheduled publication workflow.
-   * Checks every minute if there are draft articles to publish.
-   */
-
   '*/1 * * * *': async () => {
-    // fetch articles to publish;
-    const draftArticleToPublish = await strapi.entityService.findMany('plugin::strapi-plugin-fcm.fcm-notification', {
-      publicationState: 'preview', // preview returns both draft and published entries
-      filters: {
-        publishedAt: {
-          $null: true, // so we add another condition here to filter entries that have not been published
-        },
-        publish_at: {
-          $lt: new Date() // and we keep only articles with a 'publish_at' datetime value that is lower than the current datetime
-        }
-      }
-    });
-    // update the publish_at of articles previously fetched
-    await Promise.all(draftArticleToPublish.map(article => {
-      return strapi.entityService.update('plugin::strapi-plugin-fcm.fcm-notification', article.id, {
-        data: {
-          publishedAt: new Date(),
-        }
-      });
-    }))
+    await publishNotifications();
   },
   '0 10 * * *': async () => {
     await getAllRatings();
@@ -33,6 +9,29 @@ module.exports = {
     await getReviews();
   }
 };
+
+async function publishNotifications() {
+  // fetch articles to publish;
+  const draftArticleToPublish = await strapi.entityService.findMany('plugin::strapi-plugin-fcm.fcm-notification', {
+    publicationState: 'preview', // preview returns both draft and published entries
+    filters: {
+      publishedAt: {
+        $null: true, // so we add another condition here to filter entries that have not been published
+      },
+      publish_at: {
+        $lt: new Date() // and we keep only articles with a 'publish_at' datetime value that is lower than the current datetime
+      }
+    }
+  });
+  // update the publish_at of articles previously fetched
+  await Promise.all(draftArticleToPublish.map(article => {
+    return strapi.entityService.update('plugin::strapi-plugin-fcm.fcm-notification', article.id, {
+      data: {
+        publishedAt: new Date(),
+      }
+    });
+  }))
+}
 
 async function getReviews() {
   try {
@@ -99,8 +98,6 @@ async function getAllRatings() {
         const chunk = entries.slice(i, i + chunkSize);
         await getInfoForChunk(chunk)
     }
-
-
   } catch (error) {
     console.log(error)
   }
--- src/api/mcu-project/content-types/mcu-project/lifecycles.js
+++ src/api/mcu-project/content-types/mcu-project/lifecycles.js
@@ -1,6 +1,6 @@
 module.exports = {
   afterCreate(event) {
-    const { result, params } = event;
+    const { result } = event;
 
     const { id, Title, ReleaseDate, Type, Posters } = result
 
@@ -14,6 +14,7 @@ module.exports = {
           publish_at: `${ReleaseDate}T08:00:00.000Z`,
           mcu_project: [id],
           image: Posters[0].PosterUrl,
+          isReleaseNotification: true,
           payload: {
             data: {
               url: `https://mcuwidgets.page.link/mcu/${id}`
@@ -22,5 +23,43 @@ module.exports = {
         }
       })
     }
+  },
+  afterUpdate(event) {
+    const { result } = event;
+    const { Title, ReleaseDate, Type, Posters, notifications } = result
+
+    let updateNotifications = notifications
+      .filter((not) => not.isReleaseNotification)
+      .filter((not) => not.publishedAt == null)
+      .filter((not) => not.publish_at != null)
+      .filter((not) => not.publish_at > new Date().toISOString())
+
+    updateNotifications.forEach((notification) => {
+      let data = {}
+
+      if (!notification.publish_at.startsWith(`${ReleaseDate}`)) {
+        data.publish_at = `${ReleaseDate}T08:00:00.000Z`
+      }
+
+      if(Posters.length > 0 && notification.image != Posters[0].PosterUrl) {
+        data.image = Posters[0].PosterUrl
+      }
+
+      if (notification.title != Title) {
+        data.title = Title
+      }
+
+      if (notification.target != Type) {
+        data.target = Type
+      }
+
+      if (notification.title != Title || notification.target != Type) {
+        data.body = `${Title} (${Type}) releases today!`
+      }
+
+      strapi.entityService.update('plugin::strapi-plugin-fcm.fcm-notification', notification.id, {
+        data
+      })
+    })
   }
 };
