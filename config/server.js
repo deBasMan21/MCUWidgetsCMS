b3245bcaf91338bf66cd9f77520664c1cc21b3b3
deBasMan21 <bbuijsen@gmail.com>
2023-04-12 15:44:53 +0200

    add url

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ const cronTasks = require("./cron-task.js");
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://serverbuijsen.nl',
   app: {
     keys: env.array('APP_KEYS'),
   },
