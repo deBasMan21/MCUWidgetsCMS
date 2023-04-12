70ff7094d4c9c266bda310880d0759cf7499027d
deBasMan21 <bbuijsen@gmail.com>
2023-04-12 15:09:43 +0200

    add email client

--- config/plugins.js
+++ config/plugins.js
@@ -13,4 +13,19 @@ module.exports = ({ env }) => ({
     'random-sort': {
       enabled: true,
     },
+    email: {
+      config: {
+        provider: 'sendmail',
+        providerOptions: {
+          dkim: {
+            privateKey: '',
+            keySelector: 'default',
+          }
+        },
+        settings: {
+          defaultFrom: 'noreply@serverbuijsen.nl',
+          defaultReplyTo: 'bbuijsen@gmail.com'
+        }
+      }
+    }
 });
