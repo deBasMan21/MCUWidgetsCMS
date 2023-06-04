520e64bc3764b13853f656d9bd27ea1a674815c0
deBasMan21 <bbuijsen@gmail.com>
2023-06-04 19:54:48 +0200

    new test

--- config/cron-task.js
+++ config/cron-task.js
@@ -2,7 +2,7 @@ module.exports = {
   '*/1 * * * *': async () => {
     await publishNotifications();
   },
-  '50 17 * * *': async () => {
+  '56 17 * * *': async () => {
     await getAllRatings();
     console.log('executed task')
   },
