b4e09c6765916e073367525f853e3cb723eb11f6
deBasMan21 <bbuijsen@gmail.com>
2023-06-12 13:21:19 +0200

    remove task

--- CMS/config/cron-task.js
+++ CMS/config/cron-task.js
@@ -8,9 +8,6 @@ module.exports = {
   '0 10 * * * ': async () => {
     await getReviews();
   },
-  '20 11 * * *': async () => {
-    await saveEntitiesToTrackingService()
-  }
 };
 
 async function publishNotifications() {
@@ -265,100 +262,4 @@ function getObjectProperties(data) {
   return `<h2>${title}</h2>` + keys.map(key => {
     return `<strong>${key}: </strong>${data[key]}<br />`
   }).join('');
-}
-
-async function saveEntitiesToTrackingService() {
-  const fetch = require('node-fetch')
-
-  const projects = await strapi.entityService.findMany('api::mcu-project.mcu-project', {
-    populate: ['actors', 'directors', 'Posters', 'related_projects']
-  });
-  console.log(projects)
-  projects.forEach(async (project) => {
-    await createProject(project, fetch);
-  })
-}
-
-async function createProject(proj, fetch) {
-  const { id, Title, ReleaseDate, Type, Posters, Overview, imdb_id, Categories, actors, directors, related_projects } = proj
-
-  let project = {
-    id: id,
-    title: Title,
-    releaseDate: ReleaseDate ?? new Date(),
-    overview: Overview ?? "",
-    imdb_id: imdb_id ?? "",
-    categories: Categories ?? "",
-    posterUrl: Posters[0].PosterUrl,
-    type: Type,
-    actors: actors.map((actor) => { return { id: actor.id } }),
-    directors: directors.map((director) => { return { id: director.id } }),
-    relatedProjects: related_projects.map((project) => { return { id: project.id } })
-  }
-
-  console.log(project)
-  try {
-    const result = await fetch(`http://mcu-widgets-recommendations-api:3000/api/project`, {
-      method: 'post',
-      body: JSON.stringify(project),
-      headers: { 'Content-Type': 'application/json' }
-    })
-
-    console.log(result)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-async function createActor(act, fetch) {
-  const { id, FirstName, LastName, ImageUrl, DateOfBirth, Character, mcu_projects } = act
-
-  let actor = {
-    id: id,
-    firstName: FirstName,
-    lastName: LastName,
-    dateOfBirth: DateOfBirth,
-    imageUrl: ImageUrl,
-    character: Character,
-    projects: []
-  }
-
-  console.log(actor)
-  try {
-    const result = await fetch(`http://mcu-widgets-recommendations-api:3000/api/actor`, {
-      method: 'post',
-      body: JSON.stringify(actor),
-      headers: { 'Content-Type': 'application/json' }
-    })
-
-    console.log(result)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-async function createDirector(dir, fetch) {
-  const { id, FirstName, LastName, ImageUrl, DateOfBirth, mcu_projects } = dir
-
-  let director = {
-    id: id,
-    firstName: FirstName,
-    lastName: LastName,
-    dateOfBirth: DateOfBirth,
-    imageUrl: ImageUrl,
-    projects: []
-  }
-
-  console.log(director)
-  try {
-    const result = await fetch(`http://mcu-widgets-recommendations-api:3000/api/director`, {
-      method: 'post',
-      body: JSON.stringify(director),
-      headers: { 'Content-Type': 'application/json' }
-    })
-
-    console.log(result)
-  } catch (error) {
-    console.log(error)
-  }
 }
